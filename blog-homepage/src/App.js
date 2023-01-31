05ece614f85d2f84f79c70ac4e1da8a2ba1fc0bb
Antonio Aranda <aarandaeggermont@gmail.com>
2023-01-30 21:45:54 -0800

    added category sections

--- blog-homepage/src/App.js
+++ blog-homepage/src/App.js
@@ -1,38 +1,51 @@
-// import logo from './logo.svg';
 import './App.css';
 import missedArticles from './_data/missed-articles.json';
 import yourArticles from './_data/your-articles.json';
 import Article from './Article/Article';
 
-
-// https://www.pluralsight.com/guides/iterate-through-a-json-response-in-jsx-render-for-reactjs
-
-console.log(missedArticles);
-console.log(yourArticles);
-
-/*
-yourArticles.map((article) => {
-  console.log(article);
-}); */
-
 function App() {
 
   return (
     <div>
-      {yourArticles.map(article => (
- 
-        <Article 
-          author={article.author}
-          description={article.description}
-          hasAudioAvailable={article.hasAudioAvailable}
-          image={article.image}
-          link={article.link}
-          memberPreview={article.memberPreview}
-          minutesToRead={article.minutesToRead}
-          postedDate={article.postedDate}
-          title={article.title}
-        />
-      ))}
+      <div>
+        <span className='section-category'> For you </span>
+        <hr></hr>
+      </div>
+      <div>
+        {yourArticles.map(article => (
+  
+          <Article 
+            author={article.author}
+            description={article.description}
+            hasAudioAvailable={article.hasAudioAvailable}
+            image={article.image}
+            link={article.link}
+            memberPreview={article.memberPreview}
+            minutesToRead={article.minutesToRead}
+            postedDate={article.postedDate}
+            title={article.title}
+          />
+        ))}
+      </div>
+      <div>
+        <span className='section-category'> In case you missed it </span>
+        <hr></hr>
+      </div>
+      <div>
+        {missedArticles.map(article => (
+            <Article 
+            author={article.author}
+            description={article.description}
+            hasAudioAvailable={article.hasAudioAvailable}
+            image={article.image}
+            link={article.link}
+            memberPreview={article.memberPreview}
+            minutesToRead={article.minutesToRead}
+            postedDate={article.postedDate}
+            title={article.title}
+          />
+        ))}
+      </div>
     </div>
   );
 
